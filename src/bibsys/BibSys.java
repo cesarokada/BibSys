e1e5451b4000290bfdd11d3ae41d6e2e29b86dbd
Felipe Piovezan <felipevpiovezan@gmail.com>
2017-05-16 23:27:34 -0300

    Tela Inicial

--- src/bibsys/BibSys.java
+++ src/bibsys/BibSys.java
@@ -1,9 +1,14 @@
 package bibsys;
 
+import br.unesp.rc.bibSys.apresentacao.MainJFrame;
+import javax.swing.JFrame;
+
 public class BibSys {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        JFrame frame = new MainJFrame();
+        frame.pack();
+        frame.setVisible(true);
     }
     
 }
